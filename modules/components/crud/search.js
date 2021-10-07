e099d9f8fa9638c73ac64e959de77c408109c8d2
Agustin Fernandez <aaferna@outlook.com.ar>
2021-10-07 08:48:59 -0300

    Update search.js

--- modules/components/crud/search.js
+++ modules/components/crud/search.js
@@ -5,6 +5,26 @@ const   express = require('express'),
         solar = require("solardb-core"),
         isJSON = require('is-valid-json');
 
+        const getObjects = (obj, key, val) => {
+            var objects = [];
+            for (var i in obj) {
+                if (!obj.hasOwnProperty(i)) continue;
+                if (typeof obj[i] == 'object') {
+                    objects = objects.concat(getObjects(obj[i], key, val));    
+                } else 
+                //if key matches and value matches or if key matches and value is not passed (eliminating the case where key matches but passed value does not)
+                if (i == key && obj[i] == val || i == key && val == '') { //
+                    objects.push(obj);
+                } else if (obj[i] == val && key == ''){
+                    //only add if the object is not already in the array
+                    if (objects.lastIndexOf(obj) == -1){
+                        objects.push(obj);
+                    }
+                }
+            }
+            return objects;
+        }
+        
         const getValues = (obj, key) => {
             var objects = [];
             for (var i in obj) {
@@ -51,7 +71,7 @@ const   express = require('express'),
                                 elements = history.pop()
 
                                 decode = util.indexDecode(elements)
-                                detect = util.getObjects(decode, req.body.key, req.body.value)
+                                detect = getObjects(decode, req.body.key, req.body.value)
                                 histoDecode.push(
                                     {
                                         position: "pop", 
