11de41744c8e6fd2cd73a38a2e688657a9dc6da9
Agustin Fernandez <aaferna@outlook.com.ar>
2021-10-07 18:51:28 -0300

    0.11.0

--- modules/components/crud/insert.js
+++ modules/components/crud/insert.js
@@ -7,43 +7,43 @@ const   express = require('express'),
         isJSON = require('is-valid-json');
 
 
-        router.post('/insert/:collection', tokenValidator, (req, res) => {
+        router.post('/insert/:collection?', tokenValidator, (req, res) => {
 
-            if(!req.params.collection){
-                res.status(400).json({ msg: "Valide tener ingresado la Coleccion" })
-            } 
+            if((req.params.collection === undefined) || (req.params.collection === "")){
+                res.status(400).json({ msg: "Valide tener ingresado la Coleccion y el ID" }) 
+            } else { 
             
-            if(util.searchPermits(req.user.permits, req.params.collection, "create") === true || req.user.admin === true){
-                if(isJSON(req.body) && Object.keys(req.body).length !== 0){
+                if(util.searchPermits(req.user.permits, req.params.collection, "create") === true || req.user.admin === true){
+                    if(isJSON(req.body) && Object.keys(req.body).length !== 0){
 
-                    try{
+                        try{
 
-                        const insert = solar.dbInsert(
-                            jwt.encode(req.body, config.hindex),
-                            req.params.collection,
-                            config.container
-                        )
+                            const insert = solar.dbInsert(
+                                jwt.encode(req.body, config.hindex),
+                                req.params.collection,
+                                config.container
+                            )
 
-                        if(insert.id){
-                            res.status(201).json({ id: insert.id })
-                        } else { res.status(400).json({ msg: "No se pudo ingresar los datos"})  }
+                            if(insert.id){
+                                res.status(201).json({ id: insert.id })
+                            } else { res.status(400).json({ msg: "No se pudo ingresar los datos"})  }
 
-                    }catch(err){
-                        log.reg(deployPath, "No se pudo ingresar los datos : "+ err)
-                        res.status(500).json({ msg: "No se pudo ingresar los datos"}) 
+                        }catch(err){
+                            log.reg(deployPath, "No se pudo ingresar los datos : "+ err)
+                            res.status(500).json({ msg: "No se pudo ingresar los datos"}) 
+                        }
+
+                    } else { 
+                        // log.reg(deployPath, "El JSON enviado no es Valido /insert")
+                        res.status(400).json({ msg: "El JSON enviado no es Valido"}) 
                     }
 
                 } else { 
-                    // log.reg(deployPath, "El JSON enviado no es Valido /insert")
-                    res.status(400).json({ msg: "El JSON enviado no es Valido"}) 
+                    log.reg(deployPath, "El usuario no tiene permisos de escritura /insert")
+                    res.status(401).json({ msg: "El usuario no tiene los permisos correctos"}) 
                 }
-
-            } else { 
-                log.reg(deployPath, "El usuario no tiene permisos de escritura /insert")
-                res.status(401).json({ msg: "El usuario no tiene los permisos correctos"}) 
+                        
             }
-                    
-
         })
 
 module.exports = router;
--- modules/components/crud/search.js
+++ modules/components/crud/search.js
@@ -54,8 +54,8 @@ const   express = require('express'),
 
         router.get('/search/keyvalue/:pop?', tokenValidator, (req, res) => {
 
-            if(!req.body.collection && !req.body.id){
-                res.status(400).json({ msg: "Valide tener ingresado la Coleccion" })
+            if((req.body.collection === undefined) || (req.body.id === undefined) || (req.body.collection === "") || (req.body.id === "")){
+                res.status(400).json({ msg: "Valide tener ingresado la Coleccion y el ID" }) 
             } else {
             
                 if(util.searchPermits(req.user.permits, req.body.collection, "select") === true || req.user.admin === true){
@@ -116,14 +116,14 @@ const   express = require('express'),
                                 res.status(400).json({ msg: "No se pudo encontrar los datos"})
                             }
                         }catch(err){
-                            log.reg(deployPath, "No se pudo encontrar los datos : "+ err)
+                            log.reg(deployPath, "No se pudo encontrar los datos en /search/keyvalue : "+ err)
                             res.status(500).json({ msg: "No se pudo encontrar los datos"}) 
                         }
                     } else { 
                         res.status(400).json({ msg: "El JSON enviado no es Valido"}) 
                     }
                 } else { 
-                    log.reg(deployPath, "El usuario no tiene permisos de lectura /select")
+                    log.reg(deployPath, "El usuario no tiene permisos de lectura /search/keyvalue")
                     res.status(401).json({ msg: "El usuario no tiene los permisos correctos"}) 
                 }
             }     
