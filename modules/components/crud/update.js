61ad6b196e1c09682a23af4a150f08b2255a3052
Agustin Fernandez <aaferna@outlook.com.ar>
2022-01-01 20:56:23 -0300

    1.3.4

--- modules/components/crud/update.js
+++ modules/components/crud/update.js
@@ -44,5 +44,43 @@ const   express = require('express'),
             }      
 
         })
+        router.put('/update/flush/:collection?/:id?', tokenValidator, (req, res) => {
+            if((req.params.collection === undefined) || (req.params.collection === "") || (req.params.id === undefined) || (req.params.id === "")){
+                res.status(400).json({ msg: "Valide tener ingresado la Coleccion y el ID" }) 
+            } else { 
+                if(util.searchPermits(req.user.permits, req.params.collection, "update") === true || req.user.admin === true){
+                    if(isJSON(req.body) && Object.keys(req.body).length !== 0){
 
+                        try{
+                            
+                            const r = solar.dbFlushInsert(
+                                util.indexEncode(req.body),
+                                req.params.id, 
+                                req.params.collection, 
+                                config.container
+                            )
+
+                            if(r.id){
+                                res.status(201).json({ msg: "Actualizacion realizada" })
+                            } else { 
+                                res.status(400).json({ msg: "No se pudo ingresar los datos"})
+                            }
+
+                        }catch(err){
+                            log.reg(deployPath, "No se pudo ingresar los datos : "+ err)
+                            res.status(500).json({ msg: "No se pudo ingresar los datos"}) 
+                        }
+
+                    } else { 
+                        // log.reg(deployPath, "El JSON enviado no es Valido /insert")
+                        res.status(400).json({ msg: "El JSON enviado no es Valido"}) 
+                    }
+
+                } else { 
+                    log.reg(deployPath, "El usuario no tiene permisos de escritura /insert")
+                    res.status(401).json({ msg: "El usuario no tiene los permisos correctos"}) 
+                }
+            }      
+
+        })
 module.exports = router;
